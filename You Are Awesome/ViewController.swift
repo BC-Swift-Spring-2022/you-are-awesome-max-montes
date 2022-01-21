63cdc2750a4d63666bd63f0453ab75d31c989526
max-montes <77820353+max-montes@users.noreply.github.com>
2022-01-20 19:43:14 -0500

    connecting buttons

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("👍 viewDidLoad has run!")
     }
 
 
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😎 The message button was pressed!")
+    }
 }
 
